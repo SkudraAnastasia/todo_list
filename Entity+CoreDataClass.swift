df8a45f121085f247b32d3d62a5b8cccdea04f1b
Anastasia <a1111@Anastasias-MacBook-Pro.local>
2026-04-01 22:17:12 -0700

    started CoreData

---
+++ Entity+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  Entity+CoreDataClass.swift
+//  TO - DO LIST
+//
+//  Created by Anastasia on 01.04.2026.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias EntityCoreDataClassSet = NSSet
+
+@objc(Entity)
+public class Entity: NSManagedObject {
+
+}
